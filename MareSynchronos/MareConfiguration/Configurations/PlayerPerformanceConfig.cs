b9ce34bf1056364723318779bfaf4cc2241880e5
Eauldane <elf@eauldane.com>
2025-08-26 15:53:19 +0100

    Adjusted triangle threshold for auto pause, and made notifications for syncshell members the default as well.

--- MareSynchronos/MareConfiguration/Configurations/PlayerPerformanceConfig.cs
+++ MareSynchronos/MareConfiguration/Configurations/PlayerPerformanceConfig.cs
@@ -7,9 +7,9 @@ public class PlayerPerformanceConfig : IMareConfiguration
     public int Version { get; set; } = 1;
     public bool AutoPausePlayersExceedingThresholds { get; set; } = true;
     public bool NotifyAutoPauseDirectPairs { get; set; } = true;
-    public bool NotifyAutoPauseGroupPairs { get; set; } = false;
+    public bool NotifyAutoPauseGroupPairs { get; set; } = true;
     public int VRAMSizeAutoPauseThresholdMiB { get; set; } = 500;
-    public int TrisAutoPauseThresholdThousands { get; set; } = 175;
+    public int TrisAutoPauseThresholdThousands { get; set; } = 400;
     public bool IgnoreDirectPairs { get; set; } = true;
     public TextureShrinkMode TextureShrinkMode { get; set; } = TextureShrinkMode.Default;
     public bool TextureShrinkDeleteOriginal { get; set; } = false;
